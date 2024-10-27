1fe4d5ffd6bfb7b7841c51be86fc1814356151e2
Ouassim Rk <82084028+wassim005@users.noreply.github.com>
2024-10-27 15:14:44 +0100

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+Name1 = input("Enter name1 :")
+Country = input("Enter country :")
+City = input("Enter city :")
+Name2 = input("Enter name1 :")
+Name3 = input("Enter name1 :")
+Phrase = input("Enter somthing you love to do :")
+Place1 = input("Enter palce1 :")
+Place2 = input("Enter place2 :")
+
+
+Madlibs = (f"My name is {Name1}, from {Country} exactly {City},i love {Phrase}. I have two friends, {Name2} and {Name3},\
+the first one study in {Place1} and the other one in {Place2}. And that's me {Name1} and this is my life.")
+
+print(Madlibs)
